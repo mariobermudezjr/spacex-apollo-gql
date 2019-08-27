fe484937c88965a0ac800b4df7c42c22cc6c69e2
Mario Bermudez Jr <mario.bermudez.jr@outlook.com>
2019-08-26 20:36:03 -0700

    [feature] Implement login functionality with Apollo Client

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -1,18 +1,23 @@
-import { ApolloProvider } from '@apollo/react-hooks';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Pages from './pages';
-import { resolvers, typeDefs } from './resolvers';
-
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
+
+import Pages from './pages';
+import Login from './pages/login';
+import injectStyles from './styles';
+import { resolvers, typeDefs } from './resolvers';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
   uri: 'http://localhost:4000/',
   headers: {
-    authorization: localStorage.getItem('token')
+    authorization: localStorage.getItem('token'),
+    'client-name': 'Space Explorer [web]',
+    'client-version': '1.0.0'
   }
 });
 
@@ -30,9 +35,22 @@ cache.writeData({
   }
 });
 
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
+
+injectStyles();
+
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
+    <IsLoggedIn />
   </ApolloProvider>,
   document.getElementById('root')
 );
